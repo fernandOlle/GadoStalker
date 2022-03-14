208141bbcd964e0ff2761eb24a62166948e5633d
frolle <frolle@inf.ufpel.edu.br>
2022-03-13 22:31:57 -0300

    first commit

---
+++ gadostalker_back/src/main/java/com/ufpel/cs/gadostalker/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ufpel.cs.gadostalker;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
