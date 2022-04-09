810f5dc52f1803c4b91579a672ad11eda5eefc3a
frolle <frolle@inf.ufpel.edu.br>
2022-04-09 16:34:59 -0300

    remove back

--- gadostalker_back/src/main/java/com/ufpel/cs/gadostalker/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.ufpel.cs.gadostalker;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
