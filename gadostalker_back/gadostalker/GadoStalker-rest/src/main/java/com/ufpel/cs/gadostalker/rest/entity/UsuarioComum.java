c9a775bb93347583b8d5887a78f4bfdbd3dd5c35
thomazio <thomazio@mertins.com.br>
2022-04-13 22:15:37 -0300

    Usuario comum e mudanca na persistence

---
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/entity/UsuarioComum.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufpel.cs.gadostalker.rest.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author thomazio
+ */
+@Entity
+@Table(name = "usuarioComum")
+@SequenceGenerator(name = "seqUsuarioComum", sequenceName = "SEQUSUARIOCOMUM", allocationSize = 1)
+public class UsuarioComum extends Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seqUsuarioComum")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UsuarioComum other = (UsuarioComum) obj;
+        return Objects.equals(this.id, other.id);
+    }
+}
