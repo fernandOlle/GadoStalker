5f652d764405975fadbded2d2bae6a4d45a864a8
thomazio <thomazio@mertins.com.br>
2022-04-15 15:22:41 -0300

    Fix imports usuariocomum

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/entity/UsuarioComum.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/entity/UsuarioComum.java
@@ -7,10 +7,6 @@ package com.ufpel.cs.gadostalker.rest.entity;
 import java.io.Serializable;
 import java.util.Objects;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
