90e9b7a2ccd83ea4cbef1efa4f152396582c9b0e
thouta <thouta@thouta>
2022-04-26 21:46:18 -0300

    Criado usuario DTO

---
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cd/gadostalker/dto/UsuarioDTO.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufpel.cd.gadostalker.dto;
+
+import com.ufpel.cs.gadostalker.rest.entity.Usuario;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author thouta
+ */
+
+@XmlRootElement
+public class UsuarioDTO {
+    @XmlElement public String cpf;
+    @XmlElement public String nome;
+    @XmlElement public String senha;
+    @XmlElement public String telefone;
+    @XmlElement public String email;
+    
+    public UsuarioDTO(){
+    }
+    
+    public UsuarioDTO(Usuario u) {
+        cpf = u.getCpf();
+        nome = u.getNome();
+        senha = u.getSenha();
+        telefone = u.getTelefone();
+        email = u.getEmail();
+    }
+}
