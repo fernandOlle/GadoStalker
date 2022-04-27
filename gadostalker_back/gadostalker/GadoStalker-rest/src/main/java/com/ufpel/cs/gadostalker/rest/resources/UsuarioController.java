33f3c9764f1570eb66ae113b0e91bff416a3ccab
thouta <thouta@thouta>
2022-04-26 21:47:59 -0300

    Endpoint de login, cadastro e de perguntas de seguranca

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/UsuarioController.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/UsuarioController.java
@@ -4,7 +4,12 @@
  */
 package com.ufpel.cs.gadostalker.rest.resources;
 
+import com.ufpel.cd.gadostalker.dto.UsuarioDTO;
 import com.ufpel.cs.gadostalker.rest.entity.Usuario;
+import com.ufpel.cs.gadostalker.rest.entity.UsuarioComum;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
@@ -12,6 +17,7 @@ import javax.persistence.PersistenceException;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.TypedQuery;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -38,67 +44,81 @@ public class UsuarioController {
     public UsuarioController() {
     }
 
-    @GET
+    @POST
     @Path("/login")
+    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public Response login(@Context ContainerRequestContext crc, @Context final HttpServletResponse response) {
-        String cpf = null;
-        String senha = null;
+    @Transactional
+    public Response login(@Context ContainerRequestContext crc, @Context final HttpServletResponse response, UsuarioDTO usuarioDTO) {
+
         TypedQuery<Usuario> u = em.createNamedQuery("Usuario.login", Usuario.class);
-        u.setParameter("cpf", cpf);
-        u.setParameter("senha", senha);
+        u.setParameter("email", usuarioDTO.email);
+        u.setParameter("senha", usuarioDTO.senha);
 
-        Usuario usuarioLogado = u.getSingleResult();
+        Usuario usuarioLogado;
 
-        if (usuarioLogado == null) {
+        try {
+            usuarioLogado = u.getSingleResult();
+        } catch (PersistenceException ex) {
             return Response
                     .status(Response.Status.NOT_FOUND)
                     .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Methods", "GET")
+                    .header("Access-Control-Allow-Methods", "POST")
                     .build();
         }
 
+        usuarioDTO.telefone = usuarioLogado.getTelefone();
+        usuarioDTO.nome = usuarioLogado.getNome();
+        usuarioDTO.cpf = usuarioLogado.getCpf();
+        usuarioDTO.senha = null;
+
         return Response
-                .ok(usuarioLogado)
+                .ok(usuarioDTO)
+                .status(Response.Status.ACCEPTED)
                 .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Methods", "GET")
+                .header("Access-Control-Allow-Methods", "POST")
                 .build();
     }
 
     @POST
     @Path("/cadastro")
     @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.TEXT_PLAIN})
-    public Response cadastro(@Context ContainerRequestContext crc, @Context final HttpServletResponse response) {
-        
-        Usuario usuarioNovo;
-        
-        try
-        {
-            usuarioNovo = new Usuario(/* todos os params (exceto id) */);
-            //ou usar abaixo
-            //
-//            usuarioNovo.setCpf(/* param */);
-//            usuarioNovo.setNome(/* param */);
-//            usuarioNovo.setPergunta(/* param */);
-//            usuarioNovo.setResposta(/* param */);
-//            usuarioNovo.setSenha(/* param */);
-//            usuarioNovo.setTelefone(/* param */);
-            
-            em.persist(usuarioNovo);
-        }
-        catch(PersistenceException ex)
-        {
+    @Transactional
+    public Response cadastro(@Context ContainerRequestContext crc, @Context final HttpServletResponse response, UsuarioComum usuario) {
+
+        try {
+            em.persist(usuario);
+        } catch (PersistenceException ex) {
             return Response
                     .status(Response.Status.BAD_REQUEST)
                     .header("Access-Control-Allow-Origin", "*")
-                    .header("Access-Control-Allow-Methods", "GET")
+                    .header("Access-Control-Allow-Methods", "POST")
                     .build();
         }
-        
-        
+
         return Response
-                .ok(usuarioNovo)
+                .status(Response.Status.CREATED)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "POST")
+                .build();
+    }
+
+    @Path("/getAllPerguntas")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getAllPerguntas() {
+
+        Map<Usuario.PerguntaSegurancaEnum, String> mapEnumStringPergunta = new HashMap<>();
+
+        Arrays.asList(Usuario.PerguntaSegurancaEnum.values())
+                .stream()
+                .forEach(valueEnum -> {
+                    mapEnumStringPergunta.put(valueEnum, valueEnum.getPergunta());
+                });
+
+        return Response
+                .ok(mapEnumStringPergunta)
+                .status(Response.Status.ACCEPTED)
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Methods", "GET")
                 .build();
