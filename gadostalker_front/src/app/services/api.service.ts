38e1b9137a82dd2e897a1ebc6dbcb0b2ff9d3978
Alejandrodsp <pereira_randro@hotmail.com>
2022-05-11 14:54:42 -0300

    Criei a estrutura da requisição post

--- gadostalker_front/src/app/services/api.service.ts
+++ gadostalker_front/src/app/services/api.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { interval, take, lastValueFrom } from 'rxjs';
-
+import { BaseService } from './base.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
   constructor(
     private http: HttpClient,
+    private BS: BaseService,
   ) { }
 
-  public async testeApi(){
-    let url = "http://localhost:8080/GadoStalker-rest/resources/rest";
-
-    return await lastValueFrom(this.http.get(url));
+  cadastroUsuarioComum(body: any){
+    let url = 'http://localhost:8080/GadoStalker-rest/resources/usuario/cadastro/uc';
+    //se quiser passar headers usar o base service 
+    //return this.http.post(url, body, this.BS.getHeaders()).pipe(take(1));
+    return this.http.post(url, body).pipe(take(1));
   }
 
 }
