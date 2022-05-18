03f42f252d94d519cfb94260ba2d7d0ee1e19201
thouta <thouta@thouta>
2022-05-18 20:13:13 -0300

    Endpoint de editar usuario

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/UsuarioController.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/UsuarioController.java
@@ -22,6 +22,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -188,6 +189,33 @@ public class UsuarioController {
         return Response
                 .ok(true)
                 .status(Response.Status.CREATED)
+                .build();
+    }
+    
+    @PUT
+    @Path("/cadastro/editar/{cpf}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Transactional
+    public Response editarUsuario(UsuarioDTO usuario, @PathParam("cpf") String cpf) {
+        
+        Usuario u = em.find(Usuario.class, cpf);
+        
+        u.setEmail(usuario.email);
+        u.setNome(usuario.nome);
+        u.setTelefone(usuario.telefone);
+        
+        try {
+            em.merge(u);
+        } catch (PersistenceException ex) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .build();
+        }
+        
+        return Response
+                .ok(usuario)
+                .status(Response.Status.ACCEPTED)
                 .build();
     }
 
