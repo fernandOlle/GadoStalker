578ac8cbd673ce5ffbffa7d869d04f326943dac7
¨gsutavo-f¨ <mudoguto@gmail.com>
2022-05-21 20:40:49 -0300

    Modificar fazenda

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/FazendaController.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/FazendaController.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -21,29 +22,29 @@ import javax.ws.rs.core.Response;
  */
 @Path("/fazenda")
 public class FazendaController {
-    
+
     @PersistenceContext(unitName = "gadostalker")
     private EntityManager em;
-    
+
     public FazendaController() {
     }
-    
+
     @POST
     @Path("/cadastro/{cpf}")
     @Consumes({MediaType.APPLICATION_JSON})
     @Transactional
     public Response cadastraFazenda(@PathParam("cpf") String cpf, List<FazendaDTO> fazendaDTOs) {
-        
+
         Proprietario p = em.find(Proprietario.class, cpf);
-        
+
         List<Fazenda> fazendas = new ArrayList<>();
-        
+
         fazendaDTOs.forEach(f -> {
             Fazenda fazenda = new Fazenda(f);
             fazenda.setProprietario(p);
             fazendas.add(fazenda);
         });
-        
+
         try {
             fazendas.forEach(f -> {
                 em.persist(f);
@@ -53,9 +54,38 @@ public class FazendaController {
                     .status(Response.Status.CONFLICT)
                     .build();
         }
-        
+
         return Response
                 .status(Response.Status.CREATED)
+                .build();
+    }
+
+    @POST
+    @Path("/modificar/{sncr}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Transactional
+    public Response editarFazenda(@PathParam("sncr") String sncr, FazendaDTO fazendaDTO) {
+
+        Fazenda fazenda;
+
+        try {
+            fazenda = em.find(Fazenda.class, sncr);
+        } catch (Exception e) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        }
+
+        fazenda.setNome(fazendaDTO.nome);
+        fazenda.setEmail(fazendaDTO.email);
+        fazenda.setTelefone(fazendaDTO.telefone);
+
+        em.merge(fazenda);
+
+        return Response
+                .ok(new FazendaDTO(fazenda))
+                .status(Response.Status.ACCEPTED)
                 .build();
     }
 }
