2301982c8d24c8f643406a26035b936326502a91
thouta <thouta@thouta>
2022-05-21 20:08:03 -0300

    Definido metodo de cadastro de fazendas

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/FazendaController.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/resources/FazendaController.java
@@ -1,8 +1,16 @@
 package com.ufpel.cs.gadostalker.rest.resources;
 
+import com.ufpel.cs.gadostalker.rest.dtos.FazendaDTO;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -17,6 +25,15 @@ public class FazendaController {
     public FazendaController() {
     }
     
-    
-    
+    @POST
+    @Path("/cadastro/{cpf}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Transactional
+    public Response cadastraFazenda(@PathParam("cpf") String cpf, List<FazendaDTO> fazendas) {
+        
+        
+        return Response
+                .status(Response.Status.CREATED)
+                .build();
+    }
 }
