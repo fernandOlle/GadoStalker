78d7c4fb5b83303b20bc4479612d79a5584d4fee
thouta <thouta@thouta>
2022-06-08 21:18:18 -0300

    Correcao das colunas de preco

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/entities/Anuncio.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/entities/Anuncio.java
@@ -42,7 +42,7 @@ public class Anuncio implements Serializable {
     @Column
     private String descricao;
     
-    @Column(precision=2)
+    @Column(precision = 10, scale = 2)
     private BigDecimal preco;
     
     @Column
