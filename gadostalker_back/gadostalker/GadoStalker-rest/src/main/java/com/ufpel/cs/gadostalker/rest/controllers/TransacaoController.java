dcce82aa1127fbd8e34c6345a3250ddd4a518b35
gsutavo <gperes@mertins.com.br>
2022-06-15 23:10:03 -0300

    endpoint para pegar numero de vendas por um determinado numero de meses e cpf do proprietario

--- gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/controllers/TransacaoController.java
+++ gadostalker_back/gadostalker/GadoStalker-rest/src/main/java/com/ufpel/cs/gadostalker/rest/controllers/TransacaoController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import com.ufpel.cs.gadostalker.rest.entities.Anuncio;
 import com.ufpel.cs.gadostalker.rest.entities.Transacao;
+import java.util.Calendar;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -56,31 +57,31 @@ public class TransacaoController {
                     .status(Response.Status.NO_CONTENT)
                     .build();
         }
-        
+
         List<TransacaoDTO> transacoesDTO = new ArrayList<>();
-        
+
         transacoes.forEach(transacao -> {
             TransacaoDTO transacaoDTO = new TransacaoDTO(transacao);
             transacoesDTO.add(transacaoDTO);
         });
-        
+
         return Response
                 .ok(transacoesDTO)
                 .status(Response.Status.OK)
                 .build();
     }
-    
+
     @POST
     @Path("/registrar/{id}")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     @Transactional
-    public Response registraTransacao(TransacaoDTO transacaoDTO, @PathParam("id") Long id) {       
+    public Response registraTransacao(TransacaoDTO transacaoDTO, @PathParam("id") Long id) {
         Anuncio a = em.find(Anuncio.class, id);
         Transacao t = new Transacao(transacaoDTO);
         t.setAnuncio(a);
-	t.setDataTransacao(new Date());
-	
+        t.setDataTransacao(new Date());
+
         try {
             em.persist(t);
             em.flush();
@@ -89,14 +90,62 @@ public class TransacaoController {
                     .status(Response.Status.BAD_REQUEST)
                     .build();
         }
-        
+
         transacaoDTO.dataTransacao = t.getDataTransacao();
         transacaoDTO.id = t.getId();
-        
+
         return Response
                 .ok(transacaoDTO)
                 .status(Response.Status.CREATED)
                 .build();
     }
-    
+
+    @GET
+    @Path("/numVendasUltimosMeses/{numMeses}/{cpf}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getNumVendasUltimosMeses(@PathParam("numMeses") int numMeses, @PathParam("cpf") String cpf) {
+        List<Long> arrayVendas = new ArrayList<>();
+
+        TypedQuery<Long> query = em.createQuery("select count(t) from Transacao t "
+                + "inner join t.anuncio a "
+                + "inner join a.produto p "
+                + "inner join p.fazenda f "
+                + "inner join f.proprietario u "
+                + "where u.cpf = :cpf and "
+                + "t.dataTransacao >= :dataInicial and "
+                + "t.dataTransacao <= :dataFinal",
+                Long.class);
+        query.setParameter("cpf", cpf);
+
+        Date hoje = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(hoje);
+        for (int i = 0; i < numMeses; i++) {
+            c.add(Calendar.MONTH, -1);
+            int lastDay = c.getActualMaximum(Calendar.DATE);
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            c.set(year, month, lastDay);
+            Date dataFinal = c.getTime();
+            query.setParameter("dataFinal", dataFinal);
+            c.set(year, month, 1);
+            Date dataInicial = c.getTime();
+            query.setParameter("dataInicial", dataInicial);
+            Long numVendasMesAtual = 0L;
+            try {
+                numVendasMesAtual = query.getSingleResult();
+            } catch (Exception e) {
+                return Response
+                        .status(Response.Status.BAD_REQUEST)
+                        .build();
+            }
+            arrayVendas.add(numVendasMesAtual);
+        }
+
+        return Response
+                .ok(arrayVendas)
+                .status(Response.Status.ACCEPTED)
+                .build();
+    }
+
 }
